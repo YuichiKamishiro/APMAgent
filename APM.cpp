e088ed98b3a8c8ad565d277548a0c23a5b8e2953
YuichiKamishiro <YuichiKamishiro@gmail.com>
2024-11-28 01:43:31 +0300

    Add library structure, cmake

---
+++ APM.cpp
@@ -0,0 +1,6 @@
+#pragma once
+#include "APM.hpp"
+
+void Agent::Agent::init() {
+    std::cout << "Hello World\n";
+}
---
+++ APM.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+namespace Agent {
+    class Agent {
+    public:
+        void init();
+        void stop();
+    };
+}
